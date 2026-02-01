108a1a216ccd63fb20fefe75d45fca8231a65a1f
André Deomondes <97148281+andredeomondes@users.noreply.github.com>
2026-02-01 14:13:54 -0300

    aula 2 criando controller

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/NinjaController.java
@@ -0,0 +1,15 @@
+package dev.java10x.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // indica que esta classe é um controlador REST
+@RequestMapping // mapeia a raiz das requisições
+public class NinjaController {
+
+    @GetMapping("/boasvindas") // mapeia requisições do tipo GET
+    public String boasVindas() {
+        return "Bem-vindo ao cadastro de ninjas!";
+    }
+}
