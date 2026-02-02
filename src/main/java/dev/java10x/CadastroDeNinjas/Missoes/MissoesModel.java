4e994c4bf3a827ebb49dabb4696a3cacb7fd23d2
André Deomondes <97148281+andredeomondes@users.noreply.github.com>
2026-02-02 00:07:54 -0300

    feat: coloca o lombok para abstracao de dados

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -2,11 +2,17 @@ package dev.java10x.CadastroDeNinjas.Missoes;
 
 import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MissoesModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,6 +23,8 @@ public class MissoesModel {
     // @OneToMany indica que uma missao pode ter varios ninjas
     @OneToMany(mappedBy = "missoes")
     private List<NinjaModel> ninjas;
+
+
 
 
 
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -2,18 +2,22 @@ package dev.java10x.CadastroDeNinjas.Ninjas;
 
 import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
-// JPA = Java Persistence API
-// Entity ele transforma uma classe em uma entidade do banco de dados
 @Entity
 // Table define o nome da tabela no banco de dados
-@Table(name="tb_cadastro")
+@Table(name = "tb_cadastro")
+@Data
+@NoArgsConstructor // Cria um construtor sem argumentos
+@AllArgsConstructor // Cria um construtor com todos os argumentos
 public class NinjaModel {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
@@ -21,45 +25,10 @@ public class NinjaModel {
 
     // @ManyToOne indica que um ninja tem uma unica missao
     @ManyToOne
-    @JoinColumn(name="missoes_id") // Foreing key - define a coluna que vai fazer o relacionamento
+    @JoinColumn(name = "missoes_id") // Foreing key - define a coluna que vai fazer o relacionamento
     private MissoesModel missao;
 
-    // NoArgs constructor: construtor vazio
-    public NinjaModel() {
-    }
 
-    // AllArgs constructor: construtor cheio
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-
-
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
-
+// JPA = Java Persistence API
+// Entity ele transforma uma classe em uma entidade do banco de dados
