ebabd2901414ec45e4f276f77d37bc55a4b424d2
André Deomondes <97148281+andredeomondes@users.noreply.github.com>
2026-02-03 01:57:21 -0300

    feat: cria as rotas do MissoesController.java

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,9 +1,35 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/missoes")
 public class MissoesController {
 
-    // Implementar endpoints CRUD para missões aqui
+    // ENDPOINTS CRUD PARA
+
+    // GET -- Mandar uma requisição para o servidor e receber uma resposta
+    @GetMapping("/listar")
+    public String listarMissoes() {
+        return "Lista de todas as missões.";
+    }
+
+    // POST -- Enviar dados para o servidor para criar um novo recurso
+    @PostMapping("/criar")
+    public String criarMissao() {
+        return "Missão criada com sucesso!";
+    }
 
 
+    // PUT -- Atualizar um recurso existente no servidor
+    @PutMapping("/alterar")
+    public String alterarMissao() {
+        return "Missão alterada com sucesso!";
+    }
 
+    // DELETE -- Deletar um recurso existente no servidor
+    @DeleteMapping("/deletar")
+    public String deletarMissao() {
+        return "Missão deletada com sucesso!";
+    }
 }
