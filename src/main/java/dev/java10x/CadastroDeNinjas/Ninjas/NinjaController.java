74efbf256b935d00b4ff36fd1014f954709ced4c
André Deomondes <97148281+andredeomondes@users.noreply.github.com>
2026-02-04 04:32:46 -0300

    feat: Inclui a rota de cadastro de ninjas usando metodo POST, tambem inicializa o Postman

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -33,8 +33,8 @@ public class NinjaController {
 
     // Adicionar  (CREATE)
     @PostMapping("/criar")
-    public String criarNinja() {
-        return "Ninja criado com sucesso!";
+    public NinjaModel criarNinja(@RequestBody NinjaModel ninja) {
+        return ninjaService.criarNinja(ninja);
     }
 
     // Listar todos os NINJAS (READ)
@@ -48,7 +48,6 @@ public class NinjaController {
     public NinjaModel listarNinjasPorId(@PathVariable Long id) {
         return ninjaService.listarNinjasPorId(id);
     }
-
 
     // Alterar os dados do NINJA (UPDATE)
     @PutMapping("/alterarID")
